727a9077f62b9e20644f65bf517a28b2608082a2
oscharko <o.scharkowski@oscharko.de>
2022-06-16 16:34:57 +0200

    First sample program

---
+++ src/Chapter03/FirstSample.java
@@ -0,0 +1,12 @@
+package Chapter03;
+
+/**
+ * This is the first sample program in Core Java Chapter 3
+ */
+
+public class FirstSample
+{
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
